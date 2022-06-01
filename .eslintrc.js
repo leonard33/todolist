22b4a1b09e393ce305ea62682f81f89a158a18a3
leonard33 <leonardkombo14@gmail.com>
2022-06-01 08:14:26 +0300

    fix linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:prettier/recommended",
-    "stylelint-config-standard",
     "stylelint-config-prettier"
   ],
   parserOptions: {
